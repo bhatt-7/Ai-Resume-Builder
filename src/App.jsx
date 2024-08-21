77961871fde59793971d02c066306adc9d5bcd1c
Ayush Bhatt <ayushbhatt11001100@gmail.com>
2024-08-21 15:29:28 +0530

    google signin added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
-
+import { Navigate, Outlet } from 'react-router-dom'
+import {useUser} from '@clerk/clerk-react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const {user,isLoaded,isSignedIn} = useUser();
+
+
+  if(!isSignedIn){
+    return <Navigate to={"/auth/sign-in"} />
+  }
 
   return (
     <>
-      <Button>Hello</Button>
+      <Outlet/>
     </>
   )
 }
